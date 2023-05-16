9ee3e7e5a4d249f516c35dd6d9c65ac41830c51a
Simran <104500089+Simransonii@users.noreply.github.com>
2023-05-16 16:01:33 +0530

    Add files via upload

---
+++ Auto-Msg-Send-on-Whatsapp/program.py
@@ -0,0 +1,11 @@
+import pywhatkit as kit
+
+#Targeted Phone no
+number='+91987654321'
+
+#Message to be sent
+message='Hey There'
+
+#Method
+# send msg to someone at 3:35 PM 
+kit.sendwhatmsg(number,message,15,35)
